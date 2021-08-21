8898306e9e792e8c2e2c0b6e171cdc8b4df1214c
Jean-MarcCheynier <jmcheynier@gmail.com>
2021-08-21 09:08:40 +0200

    Package as TypeScript module

---
+++ src/bot/dialog/contents/Button.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+exports.ButtonType = void 0;
+var ButtonType;
+(function (ButtonType) {
+    ButtonType["POSTBACK"] = "postback";
+    ButtonType["WEB_URL"] = "web_url";
+    ButtonType["PHONE_NUMBER"] = "phonenumber";
+    ButtonType["TRIGGER_SKILL"] = "trigger_skill";
+})(ButtonType = exports.ButtonType || (exports.ButtonType = {}));
---
+++ src/bot/dialog/contents/Card.js
@@ -0,0 +1,17 @@
+"use strict";
+exports.__esModule = true;
+exports.CardAttributeType = exports.CardStatusState = void 0;
+var CardStatusState;
+(function (CardStatusState) {
+    CardStatusState["DEFAULT"] = "";
+    CardStatusState["NONE"] = "none";
+    CardStatusState["INFO"] = "information";
+    CardStatusState["ERROR"] = "error";
+    CardStatusState["SUCCESS"] = "success";
+    CardStatusState["WARNING"] = "warning";
+})(CardStatusState = exports.CardStatusState || (exports.CardStatusState = {}));
+var CardAttributeType;
+(function (CardAttributeType) {
+    CardAttributeType["LINK"] = "link";
+    CardAttributeType["TEXT"] = "text";
+})(CardAttributeType = exports.CardAttributeType || (exports.CardAttributeType = {}));
